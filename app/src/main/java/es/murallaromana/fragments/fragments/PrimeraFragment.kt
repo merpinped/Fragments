26f0406e2ef609c6266cb567f303c14b61884210
merpinped <tics.dima.mera.p@gmail.com>
2021-12-10 13:05:53 +0100

    b

--- app/src/main/java/es/murallaromana/fragments/fragments/PrimeraFragment.kt
+++ app/src/main/java/es/murallaromana/fragments/fragments/PrimeraFragment.kt
@@ -5,10 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import es.murallaromana.fragments.R
 
 class PrimeraFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_primera, container, false)
+        val view = inflater.inflate(R.layout.fragment_primera, container, false)
+
+        val btnIrOtroFragment: Button = view.findViewById(R.id.btn_ir_al_segundo_fragment)
+
+        btnIrOtroFragment.setOnClickListener() {
+            val ft = activity?.supportFragmentManager?.beginTransaction()
+
+            ft?.replace(R.id.contenedor_fragments, SegundoFragment())
+            ft?.addToBackStack(null)
+            ft?.commit()
+        }
+
+        return view
     }
 }
---
+++ app/src/main/java/es/murallaromana/fragments/fragments/SegundoFragment.kt
@@ -0,0 +1,17 @@
+package es.murallaromana.fragments.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import es.murallaromana.fragments.R
+
+class SegundoFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_segundo, container, false)
+
+        return view
+    }
+}
